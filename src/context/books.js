0b7e5f7d90985cc347ef9a6f1f9e5a1a43df93c5
Dhruv Mohindru <dmohindru@gmail.com>
2023-01-23 19:25:02 +1100

    Finished video 133

--- src/context/books.js
+++ src/context/books.js
@@ -1,6 +1,24 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
+// Create Context object
 const BooksContext = createContext();
 
+// Create a provider (wrapper around context object)
+function Provider({ children }) {
+    const [count, setCount] = useState(5);
+
+    const valueToShare = {
+        count,
+        incrementCount: () => {
+            setCount(count + 1);
+        }
+    };
+
+    return <BooksContext.Provider value = {valueToShare}>
+        {children}
+    </BooksContext.Provider>
+}
+
+export { Provider };
 
 export default BooksContext;
