6b82a9a2958149d88286f4eb64f7961f110ef3e7
Dhruv Mohindru <dmohindru@gmail.com>
2023-01-24 19:14:51 +1100

    Finished Video 140

--- src/App.js
+++ src/App.js
@@ -1,73 +1,24 @@
-import axios from 'axios';
-import {useState, useEffect} from 'react';
+import {useEffect, useContext} from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import BooksContext from './context/books'
 
 function App() {
 
-    const [books, setBooks] = useState([]);
-
-    const fetchBooks = async () => {
-        const response = await axios.get('http://localhost:3001/books');
-
-        setBooks(response.data);
-    }
+    const { fetchBooks } = useContext(BooksContext);
 
     // This function is always when App component is rendered first time.
     useEffect(() => {
         fetchBooks();
     }, []);
 
-    // Edit a book by id
-    const editBookById = async (id, newTitle) => {
-        const resposne = await axios.put(`http://localhost:3001/books/${id}`, {
-            title: newTitle
-        });
-
-        console.log(resposne);
-
-        const updatedBooks = books.map((book) => {
-            if (book.id === id) {
-                return {...book, ...resposne.data};
-            }
-            return book;
-        });
-
-        setBooks(updatedBooks);
-    };
-
-    // Delete a book by id
-    const deleteBookById = async (id) => {
-        await axios.delete(`http://localhost:3001/books/${id}`);
-        
-        const updatedBooks = books.filter((book) => {
-            return book.id !== id;
-        });
-
-        setBooks(updatedBooks);
-    }
-
-    // Create book handler function
-    const createBook = async (title) => {
-
-        const response = await axios.post('http://localhost:3001/books', {
-            title
-        });
-
-        
-        const updatedBooks = [
-            ...books,
-            response.data
-        ];
-
-        setBooks(updatedBooks);
-    }
+    
 
     return (
     <div className='app'>
         <h1>Reading List</h1>
-        <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
-        <BookCreate onCreate={createBook}  /> 
+        <BookList />
+        <BookCreate /> 
     </div>
     );
 }
--- src/context/books.js
+++ src/context/books.js
@@ -1,18 +1,73 @@
 import { createContext, useState } from 'react';
+import axios from 'axios';
 
 // Create Context object
 const BooksContext = createContext();
 
 // Create a provider (wrapper around context object)
 function Provider({ children }) {
-    const [count, setCount] = useState(5);
+
+    const [books, setBooks] = useState([]);
+
+    // Fetch all books
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+
+        setBooks(response.data);
+    }
+
+    // Edit a book by id
+    const editBookById = async (id, newTitle) => {
+        const resposne = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle
+        });
+
+        const updatedBooks = books.map((book) => {
+            if (book.id === id) {
+                return {...book, ...resposne.data};
+            }
+            return book;
+        });
+
+        setBooks(updatedBooks);
+    };
+
+    // Delete a book by id
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
+        
+        const updatedBooks = books.filter((book) => {
+            return book.id !== id;
+        });
+
+        setBooks(updatedBooks);
+    }
+
+    // Create book handler function
+    const createBook = async (title) => {
+
+        const response = await axios.post('http://localhost:3001/books', {
+            title
+        });
+
+        
+        const updatedBooks = [
+            ...books,
+            response.data
+        ];
+
+        setBooks(updatedBooks);
+    }
 
     const valueToShare = {
-        count,
-        incrementCount: () => {
-            setCount(count + 1);
-        }
+        books,
+        deleteBookById,
+        editBookById,
+        fetchBooks,
+        createBook
+        
     };
+
 
     return <BooksContext.Provider value = {valueToShare}>
         {children}
